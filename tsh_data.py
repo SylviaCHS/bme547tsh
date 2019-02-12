943c69cd3f85a6b4cb5cf573aa01a41128bd8147
Huisi Cai <sylvia@huisimatombp.home>
2019-02-12 17:59:13 -0500

    Delete repetitive function and fixed typo

--- tsh_data.py
+++ tsh_data.py
@@ -23,31 +23,6 @@ def create_person(first, last, age, gender, diagnosis, results):
 
 
 def diagnose_tsh(results):
-    """Give diagnosis to each patient baseddef create_person(first, last, age, gender, diagnosis, results):
-
-
-    Args:
-        first (str): first name
-        last (str): last name
-        age (float): age
-        gender (str): Female/Male
-        diagnosis (str): whether the patient is "hyperthyroidism",
-                        "hypothyroidism", or has "normal thyroid function"
-        results (list): a list of all the test results
-
-    Returns:
-        dict: the stored patient information
-    """
-    new_person = {"First Name": first,
-                  "Last Name": last,
-                  "Age": age,
-                  "Gender": gender,
-                  "Diagnosis": diagnosis,
-                  "TSH": results}
-    return new_person
-
-
-def diagnose_tsh(results):
     """Give diagnosis to each patient based on his/her TSH test results
 
         - "hyperthyroidism" as defined by any of
@@ -78,26 +53,17 @@ def diagnose_tsh(results):
 
 
 def input_data():
-    """Give diagnosis to each patient baseddef create_person(first, last, age, gender, diagnosis, results):
-
-
-    Returns:
-        first (str): first name
-        last (str): last name
-        age (float): age
-        gender (str): Female/Male
-        diagnosis (str): whether the patient is "hyperthyroidism",
-                        "hypothyroidism", or has "normal thyroid function"
-        results (list): a list of all the test results
-
-    """
     with open("test_data.txt") as f:
         i = -1
         while (1):  # Go through .txt file to collect patient info
             lines = []
-            for i in range(i, i+4):  # Read four lines, aka one patient, at a time
+
+            # Read four lines, aka one patient, at a time
+            for i in range(i, i+4):
                 lines.append(f.readline().strip())
-            if any("END" in s for s in lines):  # Break loop when reaching the end
+
+            # Break loop when reaching the end
+            if any("END" in s for s in lines):
                 break
 
             # Name
@@ -121,8 +87,6 @@ def input_data():
 
             i += 1
             print(first, last, age, gender, diagnosis, results)
-
-
 
 
 if __name__ == "__main__":
