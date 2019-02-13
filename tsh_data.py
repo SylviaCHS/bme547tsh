d4a021a55b6d456c5d5d2adc6783a2b2afa683eb
Huisi Cai <sylvia@HuisimatoMacBook-Pro.local>
2019-02-13 13:36:28 -0500

    Added docstrings to each functions

--- tsh_data.py
+++ tsh_data.py
@@ -1,5 +1,5 @@
 def create_person(first, last, age, gender, diagnosis, results):
-    """Create dictionary with patient's information
+    """Create dictionary of each patient's information
 
     Args:
         first (str): first name
@@ -11,7 +11,7 @@ def create_person(first, last, age, gender, diagnosis, results):
         results (list): a list of all the test results
 
     Returns:
-        dict: the stored patient information
+        new_person (dict): the stored patient information
     """
     new_person = {"First Name": first,
                   "Last Name": last,
@@ -24,6 +24,19 @@ def create_person(first, last, age, gender, diagnosis, results):
 
 def diagnose_tsh(results):
     """Give diagnosis to each patient based on his/her TSH test results
+
+        Hypothroidism and hyperthyroidism are conditions where the thyroid
+        gland produces either too little (hypo) or too much (hyper) of the
+        hormone thyroxine.
+
+        The thyroid gland is under the control of the pituitary gland. When
+        the level of thyroxine drops too low, the pituitary gland produces
+        Thyroid Stimulating Hormone (TSH) which stimulates the thyroid gland
+        to produce more hormones. If the thyroid produces too little
+        thyroxine, the amount of TSH produced by the pituitary gland is
+        very high. If the thyroid produces too much thyroxine, the pituitary
+        gland produces very little TSH. Therefore, TSH levels are often used
+        to diagnose thyroid gland issues.
 
         - "hyperthyroidism" as defined by any of
           their tests results being less than 1.0,
@@ -53,6 +66,21 @@ def diagnose_tsh(results):
 
 
 def output_data(person):
+    """Output each patient's information from dictionary as an .json file
+
+    Args:
+        person (dict): the stored patient information
+
+    Returns:
+        FirstName-LastName.json: Create a new .json file containing
+                                 the following patient's information
+                                 - First Name
+                                 - Last Name
+                                 - Age
+                                 - Gender
+                                 - Diagnosis
+                                 - TSH (in ascending order)
+    """
     import json
     first = person["First Name"]
     last = person["Last Name"]
@@ -63,6 +91,23 @@ def output_data(person):
 
 
 def input_data():
+    """Input patient's information from a .txt file
+    The .txt file is in the following format:
+    - The first line has the first and last name of
+      the patient separated by a space.
+    - The second line contains the age of the patient.
+    - The third line contains the gender of the patient.
+    - The fourth line contains the name of test, followed by a comma,
+      and then a list of test results separated by commas.
+    - The file is ended with the last line having the word "END"
+
+    Args:
+        person (dict): the stored patient information
+
+    Returns:
+        FirstName-LastName.json: Create a new .json file containing
+                                 each patient's information
+    """
     with open("test_data.txt") as f:
         i = -1
         while 1:  # Go through .txt file to collect patient info
